b8e0c17140185b6214360d211944e8a35c4e98c9
vijay <vgarisha@gmail.com>
2025-10-04 12:49:02 +0530

    hello world

---
+++ 01.helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello, World!"
+echo "This is a simple shell script."
+
+
+# End of script
+# This script prints "Hello, World!" to the console.
+# To run this script, save it to a file named 01.helloworld.sh,
+# give it execute permissions with `chmod +x 01.helloworld.sh`,
+# and then execute it with `./01.helloworld.sh`.
+# Enjoy!
